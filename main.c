b09107d917ae9c66b75cc2324e7ff800e1a109e9
Konstantin Tutsch <mail@konstantintutsch.com>
2024-03-10 15:33:34 +0000

    Betty style (#2)
    * Correct spacing and comments between functions
    
    * Remove space between function name and (
    
    * Indentation for switch case statements

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 /**
  * Copyright (c) 2023-2024 Konstantin Tutsch.
- * 
- * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
  *
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
@@ -31,25 +31,25 @@
 #define DIR_LENGTH 256
 
 
-int e_arguments = 0;
-int d_arguments = 0;
+int extension_arguments = 0;
+int directory_arguments = 0;
 
 
 /**
  * cmp_extensions - Checks if string ends with string from array of strings
+ * 
  * @arg1: string to check
  * @arg2: valid extensions
  *
- * Return - 0 Success
- *          1 Failure
+ * @return: 0 -> Success / 1 -> Failure
  */
 
-int cmp_extensions (char  *file_name,
-                    char **extensions)
+int cmp_extensions(char  *file_name,
+                   char **extensions)
 {
-    for (int i = 0; i < e_arguments; i++)
+    for (int i = 0; i < extension_arguments; i++)
     {
-        if (strncmp (file_name + strlen(file_name) - strlen(extensions[i]), /* End of file_name minus extension length -> extension as string */
+        if (strncmp(file_name + strlen(file_name) - strlen(extensions[i]), /* End of file_name minus extension length -> extension as string */
                      extensions[i], /* Compare to actual extension */
                      strlen(extensions[i]) /* Only compare actual extension */) != 0)
             continue;
@@ -60,13 +60,13 @@ int cmp_extensions (char  *file_name,
     return (1);
 }
 
-
 /**
- * arg_value - Filter out the value from a command line argument 
+ * arg_value - Filter out the value from a command line argument
  *             e. g. `-d./src` -> `./src`
+ *
  * @arg1: command line argument
  *
- * Return - String
+ * @return: value of the argument
  */
 
 char *arg_value(char *argument)
@@ -81,19 +81,19 @@ char *arg_value(char *argument)
     char *value = malloc(strlen(argument));
 
     /* Write string to value, skip first two characters */
-    memcpy (value, argument + 2, strlen(argument));
+    memcpy(value, argument + 2, strlen(argument));
     /* End string */
     value[strlen(argument)] = 0;
 
     return (value);
 }
 
-
 /**
  * file_size - Get file size of file
+ *
  * @arg1: file path
  *
- * Return - Long Int
+ * @return: size of the given file
  */
 
 long int file_size(char *path)
@@ -116,12 +116,12 @@ long int file_size(char *path)
     return (size);
 }
 
-
 /**
  * count_lines - Count how many lines a file has
+ *
  * @arg1: file path
  *
- * Return - Int
+ * @return: line count of the given file
  */
 
 int count_lines(char *path)
@@ -129,7 +129,7 @@ int count_lines(char *path)
     int lines = 0;
 
     FILE *file;
-    char *buffer = malloc (file_size(path));
+    char *buffer = malloc(file_size(path));
     size_t llen = 0;
     ssize_t lread;
 
@@ -142,51 +142,50 @@ int count_lines(char *path)
 
     while ((lread = getline(&buffer, &llen, file)) != -1)
         lines++;
-                
-    fclose (file);
+
+    fclose(file);
     free(buffer);
     return (lines);
 }
 
-
 /**
  * main - Entry point of code
  *
- * Return - 0 Success
+ * @return: 0 -> Success
  */
 
-int main (int   argc,
-          char *argv[])
+int main(int   argc,
+         char *argv[])
 {
     unsigned long total_lines = 0;
-   
+
     /**
      * Count argument types for dynamic assigning
-     * e_arguments and d_arguments are definde globally -> access in every for loop
+     * extension_arguments and directory_arguments are definde globally -> access in every for loop
      */
     for (int i = 0; i < argc; i++)
     {
         switch (argv[i][1])
         {
-            case EXT_PREFIX:
-                e_arguments++;
-                break;
-            case DIR_PREFIX:
-                d_arguments++;
-                break;
+        case EXT_PREFIX:
+            extension_arguments++;
+            break;
+        case DIR_PREFIX:
+            directory_arguments++;
+            break;
         }
     }
     /* Dynamic assigning of arrays of strings */
     char **extensions;
 
-    extensions = malloc(e_arguments * sizeof(char*)); /* allocate space for e_arguments char* pointers*/
-    for (int i = 0; i < e_arguments; i++)
+    extensions = malloc(extension_arguments * sizeof(char *)); /* allocate space for extension_arguments char* pointers*/
+    for (int i = 0; i < extension_arguments; i++)
         extensions[i] = malloc(EXT_LENGTH + 1); /* allocate space for EXT_LENGTH + '\0' */
 
     char **directories;
 
-    directories = malloc(d_arguments * sizeof(char*));
-    for (int i = 0; i < d_arguments; i++)
+    directories = malloc(directory_arguments * sizeof(char *));
+    for (int i = 0; i < directory_arguments; i++)
         directories[i] = malloc(DIR_LENGTH + 1);
 
 
@@ -203,16 +202,16 @@ int main (int   argc,
 
         switch (argv[i][1])
         {
-            case EXT_PREFIX:
-                strcpy(extensions[counter_exts], value_buffer);
-                counter_exts++;
-                break;
-            case DIR_PREFIX:
-                strcpy(directories[counter_dirs], value_buffer);
-                counter_dirs++;
-                break;
-            default:
-                printf("Unknown argument type '%c', skipping.\n", argv[i][1]);
+        case EXT_PREFIX:
+            strcpy(extensions[counter_exts], value_buffer);
+            counter_exts++;
+            break;
+        case DIR_PREFIX:
+            strcpy(directories[counter_dirs], value_buffer);
+            counter_dirs++;
+            break;
+        default:
+            printf("Unknown argument type '%c', skipping.\n", argv[i][1]);
         }
 
         free(value_buffer); /* free local variable from function arg_value() */
@@ -220,15 +219,15 @@ int main (int   argc,
 
 
     /* List all recognized file types */
-    printf ("File extensions:");
+    printf("File extensions:");
 
-    for (int i = 0; i < e_arguments; i++)
-        printf (" %s", extensions[i]);
+    for (int i = 0; i < extension_arguments; i++)
+        printf(" %s", extensions[i]);
 
-    printf ("\n");
+    printf("\n");
 
 
-    for (int i = 0; i < d_arguments; i++)
+    for (int i = 0; i < directory_arguments; i++)
     {
         DIR *directory;
         struct dirent *element;
@@ -236,31 +235,31 @@ int main (int   argc,
         directory = opendir(directories[i]);
         if (directory == NULL)
         {
-            printf ("\nError opening directory %s: %s\n", directories[i], strerror(errno));
+            printf("\nError opening directory %s: %s\n", directories[i], strerror(errno));
             continue;
         }
 
-        printf ("\nDirectory %s\n", directories[i]);
+        printf("\nDirectory %s\n", directories[i]);
 
         while ((element = readdir(directory)) != NULL)
         {
-            if (cmp_extensions (element->d_name, extensions) != 0) /* files extension doesn't match */
+            if (cmp_extensions(element->d_name, extensions) != 0) /* files extension doesn't match */
                 continue;
 
             int lines;
             char path[DIR_LENGTH + BASE_LENGTH + EXT_LENGTH];
 
-            sprintf (path, "%s/%s", directories[i], element->d_name); /* merge directory path and whole file name */
+            sprintf(path, "%s/%s", directories[i], element->d_name); /* merge directory path and whole file name */
             lines = count_lines(path);
 
             if (lines == -1) /* could not get lines */
                 continue;
-            
-            printf ("- %s: %d\n", element->d_name, lines);
+
+            printf("- %s: %d\n", element->d_name, lines);
             total_lines += lines;
         }
     }
 
-    printf ("\nTotal lines of code: %ld\n", total_lines);
+    printf("\nTotal lines of code: %ld\n", total_lines);
     return (0);
 }
